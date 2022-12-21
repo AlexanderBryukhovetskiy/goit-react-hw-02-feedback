0116e246b8e9ad7dd9cf2c11c1c9671c3465f6ce
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-12-21 11:47:35 +0200

    виправлення помилок після рецензії

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import React from "react";
 import css from './Notification.module.css';
 
@@ -5,6 +6,10 @@ const Notification = ({ message }) => {
     return (
             <h2 className={css.message}>{message}</h2>
     )
+};
+
+Notification.propTypes = {
+    message: PropTypes.string.isRequired
 };
 
 export default Notification;
